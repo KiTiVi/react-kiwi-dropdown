8169ada781f29d958d0ec4cef4abbf11054f50c5
Kim Vigren <kim.vigren@gmail.com>
2019-07-10 18:58:58 +0200

    clean up example_1

--- src/example_1.js
+++ src/example_1.js
@@ -1,6 +1,5 @@
-export const example_1 = `import React, { useState, useEffect } from 'react'
+export const example_1 = `import React, { useState } from 'react'
 import Dropdown from 'react-kiwi-dropdown'
-import logo from './images/icons8-kiwi-80.png'
 
 const options = [
   { value: 'kiwi', text: 'Select kiwis' },
@@ -25,42 +24,32 @@ function App() {
   }
 
   return (
-    <div style={{ width: '1000px', margin: '0 auto' }}>
-      <header style={{ display: 'flex', alignItems: 'baseline' }}>
-        <img alt="icons8-kiwi-80.png" src={logo} />
-        <h1>react-kiwi-dropdown</h1>
-      </header>
-
-      <h2>Example #1</h2>
-      <section style={{ display: 'flex' }}>
-        <section style={{ display: 'flex' }}>
-          <aside style={{ marginRight: '40px' }}>
-            <Dropdown
-              options={options}
-              onChange={onChange}
-              buttonIndicator
-              selectedOption={selectedOption}
-              checkedButtonIndicatorStyle={\`background: #000;\`}
-            />
-          </aside>
-          <main>
-            <ul style={{ listStyle: 'none', margin: 0, padding: 0 }}>
-              {fruitBaskets.map((basket, i) => (
-                <li
-                  key={i}
-                  style={{
-                    fontWeight:
-                      selectedOption === basket.type ? 'bold' : 'normal'
-                  }}
-                >
-                  {basket.text}
-                </li>
-              ))}
-            </ul>
-          </main>
-        </section>
-      </section>
-    </div>
+    <section style={{ display: 'flex' }}>
+      <aside style={{ marginRight: '40px' }}>
+        <Dropdown
+          options={options}
+          onChange={onChange}
+          buttonIndicator
+          selectedOption={selectedOption}
+          checkedButtonIndicatorStyle={\`background: #000;\`}
+        />
+      </aside>
+      <main>
+        <ul style={{ listStyle: 'none', margin: 0, padding: 0 }}>
+          {fruitBaskets.map((basket, i) => (
+            <li
+              key={i}
+              style={{
+                fontWeight:
+                  selectedOption === basket.type ? 'bold' : 'normal'
+              }}
+            >
+              {basket.text}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </section>
   )
 }
 
